6ae83216789bb57927dc8d92c9772a480f6925ec
Kolbycamp311 <kolbycamp311@gmail.com>
2021-04-11 20:14:32 -0400

    No problems in testing

---
+++ Hazy.js
@@ -0,0 +1,37 @@
+function calculate(arr) {
+    const newArr = []
+    const operand = ['+', '-', '/', '*']
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'string' || typeof arr[i] === 'number' || arr[i] === null) {
+        if (operand.includes(arr[i])) {
+          newArr.push(arr[i])
+        } else {
+          if (arr[i] !== ('')) {
+            let notStr = Number(arr[i])
+  
+            if (!isNaN(notStr)) newArr.push(notStr)
+          }
+        }
+      }
+    }
+  
+    if (newArr.length < 3) return NaN
+    let num1 = newArr[0]
+    let num2 = newArr[2]
+  
+    switch (newArr[1]) {
+      case '+':
+        return num1 + num2
+      case '-':
+        return num1 - num2
+      case '*':
+        return num1 * num2
+      case '/':
+        return num1 / num2
+      default:
+        return 'NaN'
+    }
+  }
+  
+  module.exports = calculate
